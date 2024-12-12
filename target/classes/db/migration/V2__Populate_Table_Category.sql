e94d15fbe3e80ed178000d0b15c61e8c3f4871e4
Alana Barbosa <itsalanayall@hotmail.com>
2024-12-12 11:57:57 -0300

    Remove unwated files and update .gitignore

---
+++ target/classes/db/migration/V2__Populate_Table_Category.sql
@@ -0,0 +1,5 @@
+INSERT INTO category (name, description, created_at) VALUES
+('Technology', 'Posts related to technology trends and news', NOW()),
+('Lifestyle', 'Articles about lifestyle and personal development', NOW()),
+('Health', 'Health tips and wellness advice', NOW()),
+('Education', 'Resources and news about education', NOW());
