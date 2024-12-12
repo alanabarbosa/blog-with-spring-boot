6a474da2f158c947f5e3699073e3f7f1aff0cb7d
Alana Barbosa <itsalanayall@hotmail.com>
2024-12-12 15:57:00 -0300

    Add User Endpoint

--- src/main/java/io/github/alanabarbosa/data/vo/v1/UserVO.java
+++ src/main/java/io/github/alanabarbosa/data/vo/v1/UserVO.java
@@ -3,15 +3,28 @@ package io.github.alanabarbosa.data.vo.v1;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.RepresentationModel;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
 
-public class UserVO implements Serializable {
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+@JsonPropertyOrder({"id", "first_name", "last_name","user_name", "password", "bio", "created_at", "enabled", "roles"})
+public class UserVO extends RepresentationModel<UserVO> implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @JsonProperty("id")
-    private Long id;
+	@Mapping("id")
+	@JsonProperty("id")
+    private Long key;
 
     @JsonProperty("first_name")
     private String firstName;
@@ -21,6 +34,11 @@ public class UserVO implements Serializable {
 
     @JsonProperty("user_name")
     private String userName;
+    
+	@Column(nullable = false, name = "password")
+	//@JsonIgnore
+	@JsonProperty(value = "password", access = JsonProperty.Access.WRITE_ONLY)
+	private String password;    
 
     @JsonProperty("bio")
     private String bio;
@@ -29,86 +47,120 @@ public class UserVO implements Serializable {
     private LocalDateTime createdAt;
 
     @JsonProperty("enabled")
-    private Boolean enabled;
-
- /*   @JsonProperty("roles")
-    private List<String> roles;*/
+    private Boolean enabled;    
+  
+   
+   /* @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )    
+    @JsonProperty("roles")
+    private List<RoleVO> roles; */
 
     public UserVO() {}
-
-    public UserVO(Long id, String firstName, String lastName, String userName, String bio, 
-                  LocalDateTime createdAt, Boolean enabled, List<String> roles) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.bio = bio;
-        this.createdAt = createdAt;
-        this.enabled = enabled;
-        //this.roles = roles;
+    
+   /* public List<String> getRoleNames() {
+        return roles.stream()
+            .map(RoleVO::getName)
+            .collect(Collectors.toList());
     }
+    */
+	public Long getKey() {
+		return key;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setKey(Long key) {
+		this.key = key;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public String getUserName() {
-        return userName;
-    }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getBio() {
-        return bio;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
+	public String getBio() {
+		return bio;
+	}
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
 
-    public Boolean getEnabled() {
-        return enabled;
-    }
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
 
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
+	public Boolean getEnabled() {
+		return enabled;
+	}
 
-   /* public List<String> getRoles() {
-        return roles;
-    }
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
 
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }*/
+	/*public List<RoleVO> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<RoleVO> roles) {
+		this.roles = roles;
+	}*/
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ Objects.hash(bio, createdAt, enabled, firstName, key, lastName, password, userName);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserVO other = (UserVO) obj;
+		return Objects.equals(bio, other.bio) && Objects.equals(createdAt, other.createdAt)
+				&& Objects.equals(enabled, other.enabled) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(key, other.key) && Objects.equals(lastName, other.lastName)
+				&& Objects.equals(password, other.password) 
+				&& Objects.equals(userName, other.userName);
+	}
 }
---
+++ src/main/java/io/github/alanabarbosa/util/PasswordUtil.java
@@ -0,0 +1,30 @@
+package io.github.alanabarbosa.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
+
+public class PasswordUtil {
+
+    public static String encodePassword(String rawPassword) {
+        // Define os encoders
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+
+        Pbkdf2PasswordEncoder pbkdf2Encoder =
+                new Pbkdf2PasswordEncoder(
+                        "", 8, 185000,
+                        SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+        
+        encoders.put("pbkdf2", pbkdf2Encoder);
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        return encodedPassword.replace("{pbkdf2}", "");
+    }
+}
